e6aea82c8d44863c31f974629ae9805d7ab1c6ee
Jaime Jarauta <jaime.jarauta@gmail.com>
2024-02-18 15:29:04 -0600

    init

---
+++ ROS_V2/diffDrive_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jaimejarauta/diffDrive_ws/src
+# Build directory: /home/jaimejarauta/diffDrive_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("DiffDriveRobot/simulation_environment")
