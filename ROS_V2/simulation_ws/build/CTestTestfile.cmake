f2ff25bf868d640c2bebed0260877ae854f07427
Jaime Jarauta <jaime.jarauta@gmail.com>
2024-02-28 11:28:17 -0600

    Add README 3

---
+++ ROS_V2/simulation_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jaimejarauta/simulation_ws/src
+# Build directory: /home/jaimejarauta/simulation_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("plannar_mover/plannar_mover")
