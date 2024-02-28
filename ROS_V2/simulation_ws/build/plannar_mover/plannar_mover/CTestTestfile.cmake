ba7325f9096b8620cdc60742c9845fb49edafba3
Jaime Jarauta <jaime.jarauta@gmail.com>
2024-02-28 11:27:32 -0600

    Add README 2

--- ROS_V2/simulation_ws/build/plannar_mover/plannar_mover/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jaimejarauta/simulation_ws/src/plannar_mover/plannar_mover
-# Build directory: /home/jaimejarauta/simulation_ws/build/plannar_mover/plannar_mover
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
